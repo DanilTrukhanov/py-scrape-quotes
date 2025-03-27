deda2ee8c4f380244bcd6ad9818bc83a19a7c3ec
Danylo Trukhanov <111445038+DanilTrukhanov@users.noreply.github.com>
2025-03-27 18:11:09 +0200

    Open file one time

--- app/parse.py
+++ app/parse.py
@@ -19,33 +19,30 @@ def main(output_csv_path: str) -> None:
         writer = csv.writer(file)
         writer.writerow(["text", "author", "tags"])
 
-    counter = 1
-    while counter < 11:
-        response = requests.get(URL.format(page_number=counter))
+        counter = 1
+        while counter < 11:
+            response = requests.get(URL.format(page_number=counter))
 
-        soup = BeautifulSoup(response.content, "html.parser")
+            soup = BeautifulSoup(response.content, "html.parser")
 
-        quotes = soup.select("div.quote span.text")
-        authors = soup.select("div.quote span small.author")
-        tags = soup.select("div.quote div.tags meta")
+            quotes = soup.select("div.quote span.text")
+            authors = soup.select("div.quote span small.author")
+            tags = soup.select("div.quote div.tags meta")
 
-        lst = []
-        for i in range(len(quotes)):
-            lst.append(
-                Quote(
-                    text=quotes[i].text,
-                    author=authors[i].text,
-                    tags=tags[i].get(
-                        "content"
-                    ).split(",") if tags[i].get(
-                        "content"
-                    ) != "" else []
+            lst = []
+            for i in range(len(quotes)):
+                lst.append(
+                    Quote(
+                        text=quotes[i].text,
+                        author=authors[i].text,
+                        tags=tags[i].get(
+                            "content"
+                        ).split(",") if tags[i].get(
+                            "content"
+                        ) != "" else []
+                    )
                 )
-            )
-        counter += 1
-
-        with open(output_csv_path, "a", newline="", encoding="utf-8") as file:
-            writer = csv.writer(file)
+            counter += 1
             for quote in lst:
                 writer.writerow(quote.__dict__.values())
 
